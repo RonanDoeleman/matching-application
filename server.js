a69c9a20a189751366c6b032e66d4a052bd69e89
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-03-05 20:28:23 +0100

    added contact and about

--- server.js
+++ server.js
@@ -2,21 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path');
-const ejs = require('ejs');
 
 // ******************
 // - Middleware
 // ******************
 
 app.use(express.static('public'));
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
-})
-
-
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // ******************
 // - Setup default page
@@ -24,7 +17,23 @@ app.get('/', (req, res) => {
 
 // app.get('/', function(req, res) {
 //     res.sendFile(path.join(__dirname, '/public/index.html'));
-//   });  
+//   });
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+// ******************
+// - Templating
+// ******************
+
+app.set('view engine', 'ejs');
+
+
+let ejs = require('ejs');
+let people = ['geddy', 'neil', 'alex'];
+let html = ejs.render('<%= people.join(", "); %>', {people: people});
+
 
 
 // ******************
@@ -32,16 +41,30 @@ app.get('/', (req, res) => {
 // ******************
 
 app.get('/about', (req, res) => {
-    res.send('about')
+    res.render('about', {text: 'This is the about page'})
 })
 
 app.get('/matching', (req, res) => {
     res.send('matching')
 })
 
+app.get('/contact', (req, res) => {
+  res.render('contact', {text: 'This is the contact page'})
+})
+
+
+// ******************
+// - 404 Error handler, if no page is found
+// ******************
+
 app.use((req, res, next) => {
     res.status(404).send('We did not find the page you were looking for :(');
 })
+
+
+// ******************
+// - Start Webserver
+// ******************
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
