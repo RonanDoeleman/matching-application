d5e1000b613e136fac6a929d18d6a104010a87ce
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-03-05 14:42:56 +0100

    setup default page

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express')
-const slug = require('slug')
 const app = express()
 const port = 3000
-
+const path = require('path');
+const ejs = require('ejs');
 
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello Ronan</h1>')
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });  
+
+app.get('/about', (req, res) => {
+    res.send('about')
 })
 
-app.get('/marieke', (req, res) => {
-    res.send('<h1>Hello Marieke</h1>')
-  })
+app.get('/matching', (req, res) => {
+    res.send('matching')
+})
+
+app.use((req, res, next) => {
+    res.status(404).send('We did not find the page you were looking for :(');
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
