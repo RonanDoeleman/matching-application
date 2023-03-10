851d75bf3c067971f6e23553bec62cd87ec8acbc
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-03-10 15:54:48 +0100

    Add Profile data

--- public/scripts/matches.js
+++ public/scripts/matches.js
@@ -1,26 +1,4 @@
-const express = require('express');
-const app = express();
-const port = 3000
-// const path = require('path');
-// let ejs = require('ejs');
-
-
-// ******************
-// - Middleware
-// ******************
-
-app.use(express.static('public'));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-// ******************
-// - Templating
-// ******************
-
-app.set("view engine", "ejs");
-
-app.get('/', (req, res) => {
-  const matches = [
+const matches = [
     {   id: 1,
         image: './images/marieke.jpg',
         name: 'Marieke',
@@ -68,44 +46,3 @@ app.get('/', (req, res) => {
     }
     
   ];
-
-  res.render('index', {name: 'Home page', matches})
-});
-
-
-
-
-// ******************
-// - Routing
-// ******************
-
-
-app.get('/profile', (req, res) => {
-    res.render('profile', { text: 'Ronan'})
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', {text: 'This is the about page'})
-});
-
-app.get('/contact', (req, res) => {
-  res.render('contact', {text: 'This is the contact page'})
-});
-
-// ******************
-// - 404 Error handler, if no page is found
-// ******************
-
-app.use((req, res) => {
-    res.status(404).send('We did not find the page you were looking for');
-});
-
-
-// ******************
-// - Start Webserver
-// ******************
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-});
-
