5997dba2d1d98f47c460118e9c227cd35cd87a4a
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-03-10 18:03:59 +0100

    Routing improvements

---
+++ public/routes/matches.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    const matches = [
+        {   id: 1,
+            image: './images/marieke.jpg',
+            name: 'Marieke',
+            location: 'Hoofddorp',
+            age: '22',
+            genres: 'Techno' + ' & ' + 'Tech-House',
+            favfestivals: 'Rotterdam Rave' + ', ' + 'Soenda',
+            bio: 'Exploring the world'
+        },
+        {   id: 2,
+            image: './images/amber.jpg',
+            name: 'Amber',
+            location: 'Lisse',
+            age: '20',
+            genres: 'Hip-Hop' + ' & ' + 'R&B',
+            favfestivals: 'Woo Hah' + ', ' + 'Vunzige Deuntjes',
+            bio: 'Live, Laugh, Love'
+        },
+        {   id: 3,
+            image: './images/lisa.jpg',
+            name: 'Lisa',
+            location: 'Amsterdam',
+            age: '23',
+            genres: 'Hardcore' + ' & ' + 'Industrial',
+            favfestivals: 'PRSPCT Festival' + ', ' + 'Smeerboel',
+            bio: 'Lets dance till dawn'
+        },
+        {   id: 4,
+            image: './images/charissa.jpg',
+            name: 'Charissa',
+            location: 'Alkmaar',
+            age: '24',
+            genres: 'Techno' + ' & ' + 'Urban',
+            favfestivals: 'Verknipt' + ', ' + 'Appelsap',
+            bio: 'Carpe Diem' 
+        },
+        {   id: 5,
+            image: './images/alisa.jpg',
+            name: 'Alisa',
+            location: 'Utrecht',
+            age: '27',
+            genres: 'Klassiek' + ' & ' + 'Modern',
+            favfestivals: 'Bach in Symphony XII' + ', ' + 'Candle Lights',
+            bio: 'Lets enjoy music together!' 
+        }
+        
+      ];
+    
+      res.render('matches', {name: 'Find Match', matches})
+});
+
+  
+router.get('/new', (req, res) => {
+    res.send('Match New Form')
+})
+
+router.post('/', (req, res) => {
+    res.send('Create Match')
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+const { Router } = require('express');
 const express = require('express');
+const router = express.Router();
 const app = express();
 const port = 3000
 // const path = require('path');
@@ -19,66 +21,13 @@ app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-  const matches = [
-    {   id: 1,
-        image: './images/marieke.jpg',
-        name: 'Marieke',
-        location: 'Hoofddorp',
-        age: '22',
-        genres: 'Techno' + ' & ' + 'Tech-House',
-        favfestivals: 'Rotterdam Rave' + ', ' + 'Soenda',
-        bio: 'Exploring the world'
-    },
-    {   id: 2,
-        image: './images/amber.jpg',
-        name: 'Amber',
-        location: 'Lisse',
-        age: '20',
-        genres: 'Hip-Hop' + ' & ' + 'R&B',
-        favfestivals: 'Woo Hah' + ', ' + 'Vunzige Deuntjes',
-        bio: 'Live, Laugh, Love'
-    },
-    {   id: 3,
-        image: './images/lisa.jpg',
-        name: 'Lisa',
-        location: 'Amsterdam',
-        age: '23',
-        genres: 'Hardcore' + ' & ' + 'Industrial',
-        favfestivals: 'PRSPCT Festival' + ', ' + 'Smeerboel',
-        bio: 'Lets dance till dawn'
-    },
-    {   id: 4,
-        image: './images/charissa.jpg',
-        name: 'Charissa',
-        location: 'Alkmaar',
-        age: '24',
-        genres: 'Techno' + ' & ' + 'Urban',
-        favfestivals: 'Verknipt' + ', ' + 'Appelsap',
-        bio: 'Carpe Diem' 
-    },
-    {   id: 5,
-        image: './images/alisa.jpg',
-        name: 'Alisa',
-        location: 'Utrecht',
-        age: '27',
-        genres: 'Klassiek' + ' & ' + 'Modern',
-        favfestivals: 'Bach in Symphony XII' + ', ' + 'Candle Lights',
-        bio: 'Lets enjoy music together!' 
-    }
-    
-  ];
-
-  res.render('index', {name: 'Home page', matches})
-});
-
-
-
-
 // ******************
 // - Routing
 // ******************
 
+app.get('/', (req, res) => {
+  res.render('Index', { text: 'Ronan'})
+});
 
 app.get('/profile', (req, res) => {
     res.render('profile', { text: 'Ronan'})
@@ -91,6 +40,10 @@ app.get('/about', (req, res) => {
 app.get('/contact', (req, res) => {
   res.render('contact', {text: 'This is the contact page'})
 });
+
+const userRouter = require('./public/routes/matches')
+
+app.use('/matches', userRouter)
 
 // ******************
 // - 404 Error handler, if no page is found
