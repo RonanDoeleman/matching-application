e468bc76dcbb50d967a8f13bb4f3dc5943802543
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-03-10 14:22:37 +0100

    Find match page

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 3000
-const path = require('path');
-let ejs = require('ejs');
+// const path = require('path');
+// let ejs = require('ejs');
 
 
 // ******************
@@ -14,22 +14,33 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // ******************
-// - Setup default page
-// ******************
-
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, '/public/index.html'));
-//   });
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
-})
-
-// ******************
 // - Templating
 // ******************
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  const matches = [
+    {name: 'Marieke',
+     age: '22',
+     genres: 'Techno' + 'Industrial',
+     favfestivals: 'Rotterdam Rave' + 'Smeerboel Festival'
+    },
+    {name: 'Amber',
+     age: '20',
+     genres: 'Hip-Hop' + 'Urban',
+     favfestivals: 'Woo Hah' + 'Vunzige Deuntjes'
+    },
+    {name: 'Lisa',
+     age: '23',
+     genres: 'Hardcore' + 'Industrial',
+     favfestivals: 'PRSPCT Festival' + 'Soenda'
+    }
+  ];
+
+  res.render('index', {name: 'Home page', matches})
+});
+
 
 
 
@@ -40,27 +51,23 @@ app.set('view engine', 'ejs');
 
 app.get('/profile', (req, res) => {
     res.render('profile', { text: 'Ronan'})
-})
-
-app.get('/matchmaker', (req, res) => {
-  res.render('matchmaker', { text: 'Ronan!'})
-})
+});
 
 app.get('/about', (req, res) => {
   res.render('about', {text: 'This is the about page'})
-})
+});
 
 app.get('/contact', (req, res) => {
   res.render('contact', {text: 'This is the contact page'})
-})
+});
 
 // ******************
 // - 404 Error handler, if no page is found
 // ******************
 
-app.use((req, res, next) => {
-    res.status(404).send('We did not find the page you were looking for :(');
-})
+app.use((req, res) => {
+    res.status(404).send('We did not find the page you were looking for');
+});
 
 
 // ******************
@@ -69,5 +76,5 @@ app.use((req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
 
