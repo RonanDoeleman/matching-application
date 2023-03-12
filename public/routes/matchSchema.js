6ff05a8b76a46009ad144c9816db75059055baad
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-03-12 20:05:22 +0100

    match schema (mongoose)

---
+++ public/routes/matchSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const matchSchema = new mongoose.Schema({
+  _id: String,
+  id: Number,
+  image: String,
+  name: String,
+  location: String,
+  age: String,
+  genres: [String],
+  favfestivals: [String],
+  bio: String,
+  matches: [Number]
+});
+
+const Match =  mongoose.model('Match', matchSchema, "MatchesData");
+
+module.exports ={ 
+  Match
+}
