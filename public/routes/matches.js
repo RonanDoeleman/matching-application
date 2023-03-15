d1df148a7015a01c4f8e8f32aaf3e91a314e241b
RonanDoeleman <56078226+RonanDoeleman@users.noreply.github.com>
2023-03-15 17:09:34 +0100

    added dislike feature

--- public/routes/matches.js
+++ public/routes/matches.js
@@ -1,59 +1,53 @@
 const express = require('express')
 const router = express.Router()
-
 const { Match }  = require('./matchSchema')
 
 router.get('/', async (req, res) => {
+
     try {
-        const potentialMatches = await Match.find();
-        potentialMatches.shift()
-        res.render('matches', {name: 'Find Match', potentialMatches})
+        const getUsers = await Match.find();
+        // remove first item from array (in this case it's your own user)
+        const profile =  getUsers.shift();
+        // merge two arrays together
+        const seenMatches = profile.matches.concat(profile.dislikes);
+         // check if user id's do not include the id's from matches and dislikes (seenMatches)
+        const potentialMatches = getUsers.filter(user => !seenMatches.includes(user.id))
+       
+        res.render('matches', {name: 'Find Match', potentialMatches, profile})
       } catch (error) {
         console.error(error)
       }
 });
 
-
 // Met een button kan de gebruiker matches toevoegen aan zijn/haar profiel.
-// Het object wordt in addMatch opgeslagen. Als de JSON niet 'false' is dan gebruik je
+// Het object wordt in addMatch opgeslagen. Als de "matchButtonLike" niet "undefined" is dan gebruik je
 // findOneAndUpdate om het object in de database op te slaan. In dit geval wordt hij dus
-// opgeslagen (met $addToSet) in de array 'matches' van id 6 (het gebruikersprofiel). 
+// opgeslagen (met $addToSet) in de array 'matches' van id 7 (het gebruikersprofiel). 
 
 router.post('/match', async (req, res) => {
-  const matchButton = req.body.matchButton;
-  const addMatch = JSON.parse(matchButton);
- 
-  if(addMatch !== false) {
-    try {
-       await Match.findOneAndUpdate({id: 7}, {$addToSet:{matches: addMatch}}, {
-        new: true
-      })
+  const matchButtonLike = req.body.matchButtonLike;
+  const matchButtonDislike = req.body.matchButtonDislike;
+     
+      if(matchButtonLike){
+        try {
+          await Match.findOneAndUpdate({id: 7}, {$addToSet:{matches: Number(matchButtonLike)}}, {
+            new: true
 
-    } catch(error) {
-      console.error(error)
-    }
-  
+          })
+        } catch(error) {
+          console.error(error)
+        }
+      } else {
+        try {
+          await Match.findOneAndUpdate({id: 7}, {$addToSet:{dislikes: Number(matchButtonDislike)}}, {
+              new: true
+          })
+        } catch(error) {
+          console.error(error)
+        }
+      }
     res.redirect('/matches')
-  }
-})
-
   
-// router.delete('/profile', (req, res) => {
-//     res.send('Match New Form')
-// })
- 
-// router
-//     .route('/:id')
-//     .get((req, res) => {
-//         console.log(req.match)
-//         res.send(`Get user with ID ${req.params.id}`)
-//     })
-//     .put((req, res) => {
-//         res.send(`Update user with ID ${req.params.id}`)
-//     })
-//     .delete((req, res) => {
-//         res.send(`Delete user with ID ${req.params.id}`)
-// });
-
+})
 
 module.exports =  router
